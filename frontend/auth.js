dda38fd9b7961d7ce5e25eb082e9b713267be6aa
calvinvieira <nilocalvin@outlook.com>
2026-04-05 19:14:07 -0300

    Correção no end point do front end

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,4 +1,4 @@
 const API_BASE =
   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:8080/api"
-    : "https://stratec-ti-backend.onrender.com";
+    : "https://stratec-ti-backend.onrender.com/api";
