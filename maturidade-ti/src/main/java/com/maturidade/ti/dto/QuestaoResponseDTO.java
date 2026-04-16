a3a88209ac048c6c2b920f1c872bade0a61f8b4d
calvinvieira <nilocalvin@outlook.com>
2026-04-16 20:12:59 -0300

    [Atualização-após-Roadmap]  Ajuste no backend QuestaoResponseDTO

--- maturidade-ti/src/main/java/com/maturidade/ti/dto/QuestaoResponseDTO.java
+++ maturidade-ti/src/main/java/com/maturidade/ti/dto/QuestaoResponseDTO.java
@@ -1,20 +1,61 @@
 package com.maturidade.ti.dto;
 
 public class QuestaoResponseDTO {
+
     private Long id;
     private String pergunta;
     private String categoria;
+    private String tipoAvaliacao;
     private Integer peso;
-    public QuestaoResponseDTO() {}
-    public QuestaoResponseDTO(Long id, String pergunta, String categoria, Integer peso) {
-        this.id=id; this.pergunta=pergunta; this.categoria=categoria; this.peso=peso;
+
+    public QuestaoResponseDTO() {
     }
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getPergunta() { return pergunta; }
-    public void setPergunta(String pergunta) { this.pergunta = pergunta; }
-    public String getCategoria() { return categoria; }
-    public void setCategoria(String categoria) { this.categoria = categoria; }
-    public Integer getPeso() { return peso; }
-    public void setPeso(Integer peso) { this.peso = peso; }
+
+    public QuestaoResponseDTO(Long id, String pergunta, String categoria, String tipoAvaliacao, Integer peso) {
+        this.id = id;
+        this.pergunta = pergunta;
+        this.categoria = categoria;
+        this.tipoAvaliacao = tipoAvaliacao;
+        this.peso = peso;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public void setPergunta(String pergunta) {
+        this.pergunta = pergunta;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getTipoAvaliacao() {
+        return tipoAvaliacao;
+    }
+
+    public void setTipoAvaliacao(String tipoAvaliacao) {
+        this.tipoAvaliacao = tipoAvaliacao;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Integer peso) {
+        this.peso = peso;
+    }
 }
